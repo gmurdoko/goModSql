9b05a39082dc7504bedc26685aeb64b84fe062c2
gmurdoko <galangcahyomurdoko@gmail.com>
2020-06-20 15:01:19 +0700

    create func connDB

--- main/app.go
+++ main/app.go
@@ -22,9 +22,8 @@ func main() {
 	DB_HOST = "localhost"
 	DB_PORT = "3306"
 	DB_SCHEMA = "toko_enigma"
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA)
-	db, _ := sql.Open("mysql", dataSource)
-	connCheck(db)
+	connDB(&DB_USER, &DB_PASSWORD, &DB_HOST, &DB_PORT, &DB_SCHEMA)
+
 }
 
 func connCheck(db *sql.DB) {
@@ -32,4 +31,10 @@ func connCheck(db *sql.DB) {
 		log.Fatal(err)
 	}
 	defer db.Close()
+}
+
+func connDB(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA *string) {
+	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", *DB_USER, *DB_PASSWORD, *DB_HOST, *DB_PORT, *DB_SCHEMA)
+	db, _ := sql.Open("mysql", dbSource)
+	connCheck(db)
 }
