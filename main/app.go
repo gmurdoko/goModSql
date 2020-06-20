f1aaed99bb2f5d8be07ae71eeb5de79f9f89f6c9
gmurdoko <galangcahyomurdoko@gmail.com>
2020-06-20 16:28:13 +0700

    create conn.go and getEnv.go

--- main/app.go
+++ main/app.go
@@ -4,11 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
+	DB_ENGINE,
 	DB_USER,
 	DB_PASSWORD,
 	DB_HOST,
@@ -17,13 +19,15 @@ var (
 )
 
 func main() {
-	DB_USER = "root"
-	DB_PASSWORD = "toor"
-	DB_HOST = "localhost"
-	DB_PORT = "3306"
-	DB_SCHEMA = "toko_enigma"
-	connDB(&DB_USER, &DB_PASSWORD, &DB_HOST, &DB_PORT, &DB_SCHEMA)
+	DB_ENGINE = os.Getenv("dbEngine") //mysql
+	DB_USER = os.Getenv("dbUser")     //root
+	DB_PASSWORD = os.Getenv("dbPass") //toor
+	DB_HOST = os.Getenv("dbHost")     //localhost
+	DB_PORT = os.Getenv("dbPort")     //3306
+	DB_SCHEMA = os.Getenv("dbSchema") //toko_enigma
 
+	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA)
+	connDB(&DB_ENGINE, &dbSource)
 }
 
 func connCheck(db *sql.DB) {
@@ -31,10 +35,10 @@ func connCheck(db *sql.DB) {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	fmt.Println("cie berhasil")
 }
 
-func connDB(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA *string) {
-	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", *DB_USER, *DB_PASSWORD, *DB_HOST, *DB_PORT, *DB_SCHEMA)
-	db, _ := sql.Open("mysql", dbSource)
+func connDB(DB_ENGINE, dbSource *string) {
+	db, _ := sql.Open(*DB_ENGINE, *dbSource)
 	connCheck(db)
 }
