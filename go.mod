ef094b2e3f35655421912be5ceb55c2ad1551633
gmurdoko <galangcahyomurdoko@gmail.com>
2020-06-21 19:35:43 +0700

    get environment using viper

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mySqlApp
 
 go 1.14
 
-require github.com/go-sql-driver/mysql v1.5.0 
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/spf13/viper v1.7.0
+)
--- main/app.go
+++ main/app.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"mySqlApp/utils"
-	"os"
-)
-
-var (
-	DB_ENGINE,
-	DB_USER,
-	DB_PASSWORD,
-	DB_HOST,
-	DB_PORT,
-	DB_SCHEMA string
+	"mySqlApp/config"
 )
 
 func main() {
-	DB_ENGINE = os.Getenv("dbEngine") //mysql
-	DB_USER = os.Getenv("dbUser")     //root
-	DB_PASSWORD = os.Getenv("dbPass") //toor
-	DB_HOST = os.Getenv("dbHost")     //localhost
-	DB_PORT = os.Getenv("dbPort")     //3306
-	DB_SCHEMA = os.Getenv("dbSchema") //toko_enigma
-
-	DbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA)
-	utils.ConnDB(DB_ENGINE, DbSource)
+	config.Env_conn()
 }
